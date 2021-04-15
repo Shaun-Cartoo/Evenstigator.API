d2dc0bdd028763975fd5742ceac80412ed4ffcad
Shaun-Cartoo <u11072157@tuks.co.za>
2021-04-15 04:36:22 +0000

    Querying database via EF

---
+++ Evenstigator.API/Controllers/EventLogController.cs
@@ -0,0 +1,26 @@
+﻿using Evenstigator.API.DAO;
+using Evenstigator.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Evenstigator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventLogController : Controller
+    {
+        private ETWContext _etwContext;
+        public EventLogController()
+        {
+            _etwContext = new ETWContext();
+        }
+
+        [HttpGet]
+        [Route("logs")]
+        public IEnumerable<Log> Get()
+        {
+            return _etwContext.GetAllEventLogs().ToList();
+        }
+    }
+}
---
+++ Evenstigator.API/DAO/ETWContext.cs
@@ -0,0 +1,23 @@
+﻿using Evenstigator.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Evenstigator.API.DAO
+{
+    public class ETWContext: DbContext
+    {
+        public DbSet<Log> Log { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                @"server=localhost;userid=root;pwd=admin;port=3306;database=etw;sslmode=none;");
+        }
+
+        public IEnumerable<Log> GetAllEventLogs()
+        {
+            return Log;
+        }
+    }
+}
